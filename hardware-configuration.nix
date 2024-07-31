a966a244f087e22b74ec9ce9240e31985dab440f
Aryan Singh <s.aryan0327@gmail.com>
2024-07-31 12:23:25 -0500

    Update hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,18 +8,19 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "r>
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelParams = [ "snd-intel-dspcfg.dsp_driver=1" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6f958f76-f2cf-4d69-bf40-c67c75134cca";
+    { device = "/dev/disk/by-uuid/fef3656b-8358-46fd-8b07-cae93df3dbf4";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/B3A1-BB87";
+    { device = "/dev/disk/by-uuid/A291-AD85";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
@@ -33,7 +34,15 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s20f0u2.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
-
+  
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFir>
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    # Ensure sound related settings are enabled
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Include SOF firmware
+  environment.systemPackages = with pkgs; [
+    sof-firmware
+  ];
 }
