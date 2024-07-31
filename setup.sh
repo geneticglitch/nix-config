e7af0d6e7130eb8030d375b8c9a39a57de920fee
Aryan Singh <s.aryan0327@gmail.com>
2024-07-30 20:03:44 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Check if script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# Copy configuration files
+echo "Copying configuration files..."
+cp ./configuration.nix /etc/nixos/configuration.nix
+cp ./home.nix /etc/nixos/home.nix
+
+# Copy dotfiles
+echo "Copying dotfiles..."
+cp -r ./dotfiles /etc/nixos
+
+# Add Home Manager channel
+echo "Adding Home Manager channel..."
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
+nix-channel --update
+
+# Rebuild NixOS
+echo "Rebuilding NixOS..."
+nixos-rebuild switch
+
+# Reboot
+echo "Installation complete. Rebooting in 5 seconds..."
+sleep 5
+reboot
